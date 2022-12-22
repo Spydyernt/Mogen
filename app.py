22bd3d9f5334429b956da1e88e97daf94e6c94c8
Spydyernt <121225588+Spydyernt@users.noreply.github.com>
2022-12-22 15:08:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://GITHUB-USERNAME:token@github.com/GITHUB-USERNAME/YOUR-REPO-NAME okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://Spydyernt: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@github.com/Spydyernt/Rwpo okk && cd okk && pip3 install -U -r requirements.txt && nohup rm -rf YukkiMusicBot &")
